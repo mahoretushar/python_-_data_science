669b3ec2b57da7794177b82919d24eb2018ac890
Tushar Mahore <mahoretushar@gmail.com>
2022-01-17 23:33:07 +0530

    Day 7

---
+++ 02_control_flow/06_for_loops.py
@@ -0,0 +1,8 @@
+# for number in range(3):
+#     print("Attempt", number + 1, (number + 1) * ".")
+
+# for number in range(1, 4):
+#     print("Attempt", number, number * ".")
+
+for number in range(1, 11, 2):
+    print("*" * number)
---
+++ 02_control_flow/09_iterables.py
@@ -0,0 +1,11 @@
+# for x in range(3):
+#     print(x)
+
+# for x in "Programming":
+#     print(x)
+
+for x in [1, 2, 3, 4]:
+    print(x)
+
+# for item in shopping_cart:
+#     print(item)
